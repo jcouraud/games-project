fc5b33ec227d05d7032d859b356fecd9c147d2a4
jcouraud <20691516+jcouraud@users.noreply.github.com>
2022-06-22 10:00:26 -0600

    Update results.php

--- results.php
+++ results.php
@@ -1,3 +1,44 @@
+<?php
+//File includes
+include "includes/functions.php";
+include "includes/apitoken.php";
+require_once "classes/games.php";
+
+$searchTerm = "";
+$searchBy = "";
+
+//Get form data
+if(!empty($_POST)){
+  $searchTerm = filter_input(INPUT_POST, "searchTerm", FILTER_SANITIZE_STRING);
+  $searchBy = filter_input(INPUT_POST, "searchBy", FILTER_SANITIZE_STRING);
+  $page = 1;
+}else{
+  $searchTerm = filter_input(INPUT_GET, "searchTerm", FILTER_SANITIZE_STRING);
+  $searchBy = filter_input(INPUT_GET, "searchBy", FILTER_SANITIZE_STRING);
+  $page = isset($_GET["page"]) ? filter_input(INPUT_GET, "page", FILTER_SANITIZE_STRING) : 1;
+}
+
+//Instantiate class
+$games = new games(API_TOKEN);
+$title = "";
+
+if($searchBy == "game"){
+  $games->searchGames($searchTerm, $page);
+  $title = strtoupper($searchTerm);
+}elseif($searchBy == "genre"){
+
+}elseif($searchBy == "developer"){
+
+}elseif($searchBy == "tags"){
+
+}elseif($searchBy == "publisher"){
+
+}elseif($searchBy == "platform"){
+
+}
+
+$gameData = $games->getGameData();
+?>
 <!DOCTYPE html>
 <html lang="en">
     <head>
@@ -13,8 +54,11 @@
         <title>My Video Game Project - Search Results</title>
     </head>
     <body>
-        <h1>Results for Placeholder</h1>
+        <h1>Results for <?php echo $title;?></h1>
         <div id="results">
+          <?php
+          if(!empty($gameData) && !isset($gameData->detail)){
+          ?>
           <table id="results">
             <thead>
               <tr>
@@ -26,15 +70,38 @@
               </tr>
             </thead>
             <tbody>
+              <?php
+              foreach($gameData->results as $game){
+              ?>
               <tr>
-                <td></td>
-                <td></td>
-                <td></td>
-                <td></td>
-                <td></td>
+                <td><a href="game_details.php?gameSlug=<?php echo $game->slug;?>&searchBy=<?php echo $searchBy;?>&searchTerm=<?php echo $searchTerm;?>&page=<?php echo $page;?>" title="<?php echo $game->name;?>" target="_self"><?php echo $game->name;?></a></td>
+                <td><?php echo $game->released;?></td>
+                <td><?php echo $game->rating;?></td>
+                <td><?php
+                if(!empty($game->esrb_rating)){
+                  echo $game->esrb_rating->name;
+                }
+                ?></td>
+                <td><?php
+                if(!empty($game->platforms)){
+                  foreach($game->platforms as $platform){
+                    echo $platform->platform->name . " | ";
+                  }
+                }
+                ?></td>
               </tr>
+              <?php
+              }
+              ?>
             </tbody>
           </table>
+          <?php
+          }else{
+            ?>
+            <div id="warning">No results found</div>
+            <?php
+          }
+          ?>
           <div id="nav">
             <a href="results.php" title="Previous Page" target="_self">&lt;PREVIOUS</a> |  <a href="index.php" title="New Search" target="_self">NEW SEARCH</a> | <a href="results.php" title="Next Page" target="_self">NEXT&gt;</a>
           </div>

788471b2b6c8d399665e63ec051baab994b64fa3
jcouraud <20691516+jcouraud@users.noreply.github.com>
2022-06-23 11:52:52 -0600

    Update developers.php

--- developers.php
+++ developers.php
@@ -1,3 +1,16 @@
+<?php
+//File includes
+include "includes/functions.php";
+include "includes/apitoken.php";
+require_once "classes/games.php";
+
+$page = filter_input(INPUT_GET, "page", FILTER_SANITIZE_STRING);
+
+//Retrieve developer details
+$game = new games(API_TOKEN);
+$game->getDevelopers($page);
+$developers = $game->getDevData();
+?>
 <!DOCTYPE html>
 <html lang="en">
     <head>
@@ -14,6 +27,9 @@
     </head>
     <body>
         <h1>Available Developers</h1>
+        <?php
+        if(!empty($developers)){
+        ?>
         <table>
           <thead>
             <tr>
@@ -22,12 +38,21 @@
             </tr>
           </thead>
           <tbody>
+          <?php
+        foreach ($developers->results as $developer){
+            ?>
             <tr>
-              <td><a href="results.php" title="Details for Developer" target="_self">Placeholder</a></td>
-              <td></td>
+              <td><a href="results.php?searchBy=developer&searchTerm=<?php echo $developer->slug;?>" title="Details for <?php echo $developer->name;?>" target="_self"><?php echo $developer->name;?></a></td>
+              <td><?php echo $developer->games_count;?></td>
             </tr>
+            <?php
+            }
+            ?>
           </tbody>
         </table>
+        <?php
+        }
+        ?>
         <div id="nav">
           <a href="developers.php" title="Previous Page" target="_self">< PREVIOUS</a>
           | <a href="index.php" title="New Search" target="_self">NEW SEARCH</a> |

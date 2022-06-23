17161ff00ebb618e53e10b9901dd37bbe4a06e18
jcouraud <20691516+jcouraud@users.noreply.github.com>
2022-06-23 11:31:09 -0600

    Update platforms.php

--- platforms.php
+++ platforms.php
@@ -1,3 +1,14 @@
+<?php
+//File includes
+include "includes/functions.php";
+include "includes/apitoken.php";
+require_once "classes/games.php";
+
+//Retrieve platform details
+$game = new games(API_TOKEN);
+$game->getPlatforms();
+$platforms = $game->getPlatformData();
+?>
 <!DOCTYPE html>
 <html lang="en">
     <head>
@@ -14,6 +25,9 @@
     </head>
     <body>
         <h1>Available Platforms</h1>
+        <?php
+        if(!empty($platforms)){
+        ?>
         <table>
           <thead>
             <tr>
@@ -22,12 +36,21 @@
             </tr>
           </thead>
           <tbody>
+            <?php
+            foreach($platforms->results as $platform){
+            ?>
             <tr>
-              <td><a href="results.php" title="Details for Platform" target="_self">Placeholder</a></td>
-              <td></td>
+              <td><a href="results.php?searchBy=platform&searchTerm=<?php echo $platform->id;?>" title="Details for <?php echo $platform->name;?>" target="_self"><?php echo $platform->name;?></a></td>
+              <td><?php echo $platform->games_count;?></td>
             </tr>
+            <?php
+            }
+            ?>
           </tbody>
         </table>
+        <?php
+        }
+        ?>
         <div id="nav">
           <a href="index.php" title="New Search" target="_self">NEW SEARCH</a>
         </div>
